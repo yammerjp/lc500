e26015e205f10c4ed16252ab6767aeebce357cf0
Keisuke Nakayama <me@yammer.jp>
2024-08-27 09:09:44 +0900

    JavaScript repl

--- worker/go.mod
+++ worker/go.mod
@@ -1,3 +1,5 @@
 module github.com/yammerjp/lc500/worker
 
 go 1.22.4
+
+require rogchap.com/v8go v0.9.0
--- worker/main.go
+++ worker/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+
+	v8 "rogchap.com/v8go"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	iso := v8.NewIsolate()
+	ctx := v8.NewContext(iso)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("> ")
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		script, _ := iso.CompileUnboundScript(line, "main.js", v8.CompileOptions{})
+		val, err := script.Run(ctx)
+		if err != nil {
+			fmt.Printf("ERROR: %v\n", err)
+			continue
+		}
+
+		fmt.Println(val)
+
+		fmt.Print("> ")
+	}
 }
