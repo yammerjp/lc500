5eda30e9b1fba8b3424ae6dc4e41dc18ac05d096
Keisuke Nakayama <me@yammer.jp>
2024-08-31 10:47:38 +0900

    devide module

--- worker/main.go
+++ worker/main.go
@@ -6,74 +6,11 @@ import (
 	"net/http"
 	"os"
 
-	v8 "rogchap.com/v8go"
+	lc500Vm "github.com/yammerjp/lc500/worker/vm"
 )
 
-// ロガーを整える
-
 func workerHandler(w http.ResponseWriter, r *http.Request) {
-	iso := v8.NewIsolate()
-	globalThis := v8.NewObjectTemplate(iso)
-
-	globalThis.Set("readHeader", v8.NewFunctionTemplate(iso, func(info *v8.FunctionCallbackInfo) *v8.Value {
-		val, err := v8.NewValue(iso, r.Header.Get(info.Args()[0].String()))
-		if err != nil {
-			slog.Error("failed to create value", "error", err)
-			str, err := v8.NewValue(iso, "failed to create value")
-			if err != nil {
-				slog.Error("failed to create error value, disposing isolate", "error", err)
-				iso.Dispose()
-				return nil
-			}
-			iso.ThrowException(str)
-			return nil
-		}
-		return val
-	}))
-
-	globalThis.Set("readBody", v8.NewFunctionTemplate(iso, func(info *v8.FunctionCallbackInfo) *v8.Value {
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			slog.Error("failed to read body", "error", err)
-			str, err := v8.NewValue(iso, "failed to read body")
-			if err != nil {
-				slog.Error("failed to create error value, disposing isolate", "error", err)
-				iso.Dispose()
-				return nil
-			}
-			iso.ThrowException(str)
-			return nil
-		}
-		val, err := v8.NewValue(iso, string(body))
-		if err != nil {
-			slog.Error("failed to create value", "error", err)
-			str, err := v8.NewValue(iso, "failed to create value")
-			if err != nil {
-				slog.Error("failed to create error value, disposing isolate", "error", err)
-				iso.Dispose()
-				return nil
-			}
-			iso.ThrowException(str)
-			return nil
-		}
-		return val
-	}))
-
-	globalThis.Set("setStatus", v8.NewFunctionTemplate(iso, func(info *v8.FunctionCallbackInfo) *v8.Value {
-		w.WriteHeader(int(info.Args()[0].Int32()))
-		return nil
-	}))
-
-	globalThis.Set("setHeader", v8.NewFunctionTemplate(iso, func(info *v8.FunctionCallbackInfo) *v8.Value {
-		w.Header().Set(info.Args()[0].String(), info.Args()[1].String())
-		return nil
-	}))
-
-	globalThis.Set("renderBody", v8.NewFunctionTemplate(iso, func(info *v8.FunctionCallbackInfo) *v8.Value {
-		w.Write([]byte(info.Args()[0].String()))
-		return nil
-	}))
-	ctx := v8.NewContext(iso, globalThis)
+	vm := lc500Vm.NewVM()
 
 	scriptStr, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -81,22 +18,11 @@ func workerHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to read script", http.StatusInternalServerError)
 		return
 	}
-
-	script, err := iso.CompileUnboundScript(string(scriptStr), "main.js", v8.CompileOptions{})
-	if err != nil {
-		slog.Error("failed to compile script", "error", err)
-		http.Error(w, "Failed to compile script", http.StatusInternalServerError)
-		return
-	}
-
-	val, err := script.Run(ctx)
-	if err != nil {
-		slog.Error("failed to run script", "error", err)
-		http.Error(w, "Failed to run script", http.StatusInternalServerError)
-		return
-	}
-
-	slog.Info("script ran", "value", val)
+	vm.CompileScript(string(scriptStr))
+	vm.SetContext(lc500Vm.NewVMContext(r))
+	vm.SetResponseWriter(w)
+	vm.RunScript()
+	vm.Dispose()
 }
 
 func main() {
---
+++ worker/vm/vm.go
@@ -0,0 +1,160 @@
+package vm
+
+import (
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
+
+	v8 "rogchap.com/v8go"
+)
+
+type VM struct {
+	iso            *v8.Isolate
+	ctx            *v8.Context
+	script         *v8.UnboundScript
+	responseWriter http.ResponseWriter
+}
+
+type VMContext struct {
+	request *http.Request
+}
+
+func NewVM() *VM {
+	return &VM{
+		iso: v8.NewIsolate(),
+	}
+}
+
+func NewVMContext(r *http.Request) *VMContext {
+	return &VMContext{
+		request: r,
+	}
+}
+
+func (vm *VM) CompileScript(scriptStr string) error {
+	if vm.iso == nil {
+		return errors.New("isolate not initialized")
+	}
+	if vm.script != nil {
+		return errors.New("script already compiled")
+	}
+	script, err := vm.iso.CompileUnboundScript(string(scriptStr), "main.js", v8.CompileOptions{})
+	if err != nil {
+		return err
+	}
+	vm.script = script
+	return nil
+}
+
+func (vm *VM) SetContext(vmCtx *VMContext) error {
+	if vm.iso == nil {
+		return errors.New("isolate not initialized")
+	}
+	if vm.ctx != nil {
+		return errors.New("context already set")
+	}
+	globalThis := v8.NewObjectTemplate(vm.iso)
+	err := globalThis.Set("readHeader", v8.NewFunctionTemplate(vm.iso, func(info *v8.FunctionCallbackInfo) *v8.Value {
+		val, err := v8.NewValue(vm.iso, vmCtx.request.Header.Get(info.Args()[0].String()))
+		if err != nil {
+			slog.Error("failed to create value", "error", err)
+			str, err := v8.NewValue(vm.iso, "failed to create value")
+			if err != nil {
+				slog.Error("failed to create error value, disposing isolate", "error", err)
+				vm.iso.Dispose()
+				return nil
+			}
+			vm.iso.ThrowException(str)
+			return nil
+		}
+		return val
+	}))
+	if err != nil {
+		return err
+	}
+
+	err = globalThis.Set("readBody", v8.NewFunctionTemplate(vm.iso, func(info *v8.FunctionCallbackInfo) *v8.Value {
+		body, err := io.ReadAll(vmCtx.request.Body)
+		if err != nil {
+			slog.Error("failed to read body", "error", err)
+			str, err := v8.NewValue(vm.iso, "failed to read body")
+			if err != nil {
+				slog.Error("failed to create error value, disposing isolate", "error", err)
+				vm.iso.Dispose()
+				return nil
+			}
+			vm.iso.ThrowException(str)
+			return nil
+		}
+		val, err := v8.NewValue(vm.iso, string(body))
+		if err != nil {
+			slog.Error("failed to create value", "error", err)
+			str, err := v8.NewValue(vm.iso, "failed to create value")
+			if err != nil {
+				slog.Error("failed to create error value, disposing isolate", "error", err)
+				vm.iso.Dispose()
+				return nil
+			}
+			vm.iso.ThrowException(str)
+			return nil
+		}
+		return val
+	}))
+	if err != nil {
+		return err
+	}
+
+	err = globalThis.Set("setStatus", v8.NewFunctionTemplate(vm.iso, func(info *v8.FunctionCallbackInfo) *v8.Value {
+		vm.responseWriter.WriteHeader(int(info.Args()[0].Int32()))
+		return nil
+	}))
+	if err != nil {
+		return err
+	}
+
+	err = globalThis.Set("setHeader", v8.NewFunctionTemplate(vm.iso, func(info *v8.FunctionCallbackInfo) *v8.Value {
+		vm.responseWriter.Header().Set(info.Args()[0].String(), info.Args()[1].String())
+		return nil
+	}))
+	if err != nil {
+		return err
+	}
+
+	err = globalThis.Set("renderBody", v8.NewFunctionTemplate(vm.iso, func(info *v8.FunctionCallbackInfo) *v8.Value {
+		vm.responseWriter.Write([]byte(info.Args()[0].String()))
+		return nil
+	}))
+	if err != nil {
+		return err
+	}
+
+	vm.ctx = v8.NewContext(vm.iso, globalThis)
+	return nil
+}
+
+func (vm *VM) SetResponseWriter(w http.ResponseWriter) {
+	vm.responseWriter = w
+}
+
+func (vm *VM) Dispose() {
+	vm.iso.Dispose()
+}
+
+func (vm *VM) RunScript() error {
+	if vm.ctx == nil {
+		return errors.New("context not set")
+	}
+	if vm.script == nil {
+		return errors.New("script not compiled")
+	}
+	if vm.responseWriter == nil {
+		return errors.New("response writer not set")
+	}
+	val, err := vm.script.Run(vm.ctx)
+	if err != nil {
+		return err
+	}
+	slog.Info("script ran", "value", val)
+	return nil
+}
